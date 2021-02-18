dca7f3739d21f46b7a6a69e4c8ee2f9cd7fd8cb4
Vincent Spitale <vincentgspitale@gmail.com>
2021-02-18 12:10:03 -0500

    update readme

---
+++ InventoryTests/SpaceTest.swift
@@ -0,0 +1,19 @@
+//
+//  SpaceTest.swift
+//  InventoryTests
+//
+//  Created by Vincent Spitale on 2/18/21.
+//  Copyright © 2021 Vincent Spitale. All rights reserved.
+//
+
+import XCTest
+import CoreData
+@testable import Inventory
+
+class SpaceTest: XCTestCase {
+    
+    func testConstructor() {
+        
+    }
+    
+}
